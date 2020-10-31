4d7618bbf4ca9303ece9c178b71ce77604408026
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-10-30 20:04:31 -0500

    Directivas

--- bitcoin/app.js
+++ bitcoin/app.js
@@ -2,6 +2,7 @@ new Vue({
     el: "#app",
     data: () => ({
         name: 'Bitcoin',
-        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+        changePercent: 10
     })
 })
